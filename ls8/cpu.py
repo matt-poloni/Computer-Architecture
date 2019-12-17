4224ab4e75467c133295a96af7653fd371991de6
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-12-17 15:11:15 -0500

    Implements file loading in cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,36 +7,30 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0b0] * 0b100000000 # [0] * 256 in binary, just for practice
+        self.ram = [0b0] * 0x100 # [0] * 256 in hex, just for practice
         self.reg = [0b0] * 0b1000 # [0] * 8 in binary, just for practice
         self.pc = 0b0
     
     def ram_read(self, mar):
+        if mar > 255:
+            raise Exception(f"RAM does not extend to {mar}")
         return self.ram[mar]
     
     def ram_write(self, mar, mdr):
+        if mar > 255:
+            raise Exception(f"RAM does not extend to {mar}")
         self.ram[mar] = mdr
 
     def load(self):
         """Load a program into memory."""
-
+        if len(sys.argv) < 2:
+            raise Exception("Please specify the program to load")
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as f:
+            for line in f:
+                if len(line) > 0 and line[0] in ['0', '1']:
+                    self.ram_write(address, int(line[:8], 2))
+                    address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -72,16 +66,18 @@ class CPU:
         """Run the CPU."""
         def LDI(a, b): self.reg[a] = b
         def PRN(a, b): print(self.reg[a])
+
         opcodes = {
           0b00000000: 'NOP',
           0b00000001: 'HLT',
           0b10000010: LDI,
           0b01000111: PRN
         }
+
         while (op_fn := opcodes[(ir := self.ram_read(self.pc))]) != 'HLT':
-            num_operands = ir >> 6 # Grab two highest bits
-            operand_a = self.ram_read(self.pc + 0b1)
-            operand_b = self.ram_read(self.pc + 0b10)
+            num_operands = ir >> 6 # Grab 7th/8th bits
+            if num_operands > 0: operand_a = self.ram_read(self.pc + 0b1)
+            if num_operands > 1: operand_b = self.ram_read(self.pc + 0b10)
             if op_fn != 'NOP':
                 op_fn(operand_a, operand_b)
             self.pc += (num_operands + 1)
